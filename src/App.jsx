16a60fe32691822f79f2ca39a18cfc14bd82fd58
Wiz <im.not.ali@outlook.com>
2023-12-27 15:07:31 +0330

    create Services for manage APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
+import HomePage from "./components/templates/HomePage"
 function App() {
 
   return (
-    <>
-     
-    </>
+    <HomePage/>
   )
 }
 
---
+++ src/components/modules/TableCoin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TableCoin({coins}) {
+  console.log(coins)
+  return (
+    <div>Tablecoin</div>
+  )
+}
+
+export default TableCoin
