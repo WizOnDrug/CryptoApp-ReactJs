273f44bf96fed48ea26d2e56ef3a08d732ce3317
Wiz <im.not.ali@outlook.com>
2023-12-27 16:21:19 +0330

    Add Loader with react loader spinner

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -4,17 +4,19 @@ import {getCoinList} from '../../services/cryptoApi'
 
 function HomePage() {
     const[coins,setCoins] = useState([]);
+    const [isLoading,setLoading] = useState(true);
     useEffect(()=>{
         const getData = async () => {
           const res = await fetch(getCoinList());
           const json = await res.json();
           setCoins(json);
+          setLoading(false)
         }
         getData();
     },[]);
   return (
     <div>
-        <TableCoin coins={coins}/>
+        <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   )
 }
