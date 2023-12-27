e398989bcf652ca118a8f1ce9fabf52c54a6f7ee
Wiz <im.not.ali@outlook.com>
2023-12-27 20:27:28 +0330

    Draw Chart

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,15 +1,51 @@
-import React, { useState } from 'react'
-import {convertData} from "../../helpers/convertData"
-import styles from "./Chart.module.css"
-function Chart({chart,setChart}) {
-    const [type,setType] = useState("market_caps")
-    console.log(convertData(chart,type))
+import React, { useState } from "react";
+import { convertData } from "../../helpers/convertData";
+import styles from "./Chart.module.css";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+function Chart({ chart, setChart }) {
+  const [type, setType] = useState("prices");
+  console.log(convertData(chart, type));
   return (
     <div className={styles.container}>
-        <span className={styles.cross} onClick={()=>setChart(null)}>X</span>
-        <div className={styles.chart}></div>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+            <ChartComponent data={convertData(chart, type)} type={type}/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
+
+const ChartComponent = ({data,type}) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
