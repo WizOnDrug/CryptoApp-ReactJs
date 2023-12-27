6bf419e8cc1323b0703399d1daaee6d017c82dd8
Wiz <im.not.ali@outlook.com>
2023-12-27 20:02:07 +0330

    Add convertData func

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {convertData} from "../../helpers/convertData"
 import styles from "./Chart.module.css"
 function Chart({chart,setChart}) {
+    const [type,setType] = useState("market_caps")
+    console.log(convertData(chart,type))
   return (
     <div className={styles.container}>
         <span className={styles.cross} onClick={()=>setChart(null)}>X</span>
+        <div className={styles.chart}></div>
     </div>
   )
 }
